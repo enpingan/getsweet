6b92bcd3865149385df706d358fd670d766307e1
Ed Chang <t.edward.chang@gmail.com>
2015-08-25 23:28:32 -0400

    Fixing logout route

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 
   mount Spree::Core::Engine, at: '/'
 
-  devise_scope :spree_user do
-    delete '/logout', to: 'spree/user_sessions#destroy'
-  end
+  #devise_scope :spree_user do
+  #  delete '/logout', to: 'spree/user_sessions#destroy'
+  #end
 
   mount Alchemy::Engine, at: '/'
 end
