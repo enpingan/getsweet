720ceeed0d0c1e2da82ae7e8f9a7f3c9b1896a8e
Greg Kane <gbkane@gmail.com>
2015-10-17 18:04:26 -0400

    Product Edit View. Should be variant instead

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,27 @@ class ProductsController < ApplicationController
 
   def show
     @product = Spree::Product.find(params[:id])
+    render :show
   end
+
+  def edit
+    @product = Spree::Product.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product = Spree::Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to vendor_product_url(@product.vendor_id, @product.id)
+    else
+      render :edit
+    end
+  end
+
+private
+
+def product_params #need to add all permitable attributes, just name and price for now
+  params.require(:product).permit(:name, :price)
+end
 
 end
