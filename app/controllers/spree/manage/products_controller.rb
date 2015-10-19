66222f5b797e3dee2d83909a21e996cb12ed7650
Greg Kane <gbkane@gmail.com>
2015-10-19 16:55:54 -0400

    Auth for manage pages.

--- app/controllers/spree/manage/products_controller.rb
+++ app/controllers/spree/manage/products_controller.rb
@@ -3,19 +3,24 @@ module Spree
 
 	class ProductsController < Spree::Manage::BaseController
 
+    before_action :ensure_vendor, only: [:show, :edit, :update, :destroy]
+
   def index
+    @vendor = current_vendor
     #@vendor = Vendor.find(params[:vendor_id])
     #@products = @vendor.products
-    @vendor = Vendor.first
-		@current_order = current_order
-
-    @products = Spree::Product.all
+    # @vendor = Vendor.first
+    if @vendor.orders.present?
+	    @current_order = @vendor.orders.last
+    end
+    # @products = Spree::Product.all
+    @products = @vendor.products
     render :index
   end
 
   def new
-    @product = Spree::Product.new
-
+    @vendor = current_vendor
+    @product = @vendor.products.new
   end
 
   def create
@@ -23,6 +28,8 @@ module Spree
 
   def show
     @product = Spree::Product.friendly.find(params[:id])
+    render :show
+
   end
 
   def edit
@@ -44,6 +51,11 @@ module Spree
 
   def product_params  #Add more permissions
     params.require(:product).permit(:name)
+  end
+
+  def ensure_vendor
+    @product = Spree::Product.friendly.find(params[:id])
+    redirect_to login_url unless current_vendor.id == @product.vendor_id
   end
 
 end
