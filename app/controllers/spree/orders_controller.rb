b6db272568ecb38079c92c8e93e237d06a898a4c
Greg Kane <gbkane@gmail.com>
2015-10-19 17:17:51 -0400

    Customer authentication for orders

--- app/controllers/spree/orders_controller.rb
+++ app/controllers/spree/orders_controller.rb
@@ -1,10 +1,11 @@
 module Spree
   class OrdersController < ApplicationController
     before_action :authorize_customer
+    before_action :ensure_customer, only: [:show, :edit, :update, :destroy]
 
     def index
-      @orders = Spree::Order.all
-      # @orders = Customer.find(current_spree_user.customer_id).orders
+      @orders = current_spree_user.orders
+
       render :index
     end
 
@@ -14,14 +15,16 @@ module Spree
     end
 
     def new
+      @order = current_spree_user.orders.new
     end
 
     def create
+      @order = current_spree_user.orders.new
     end
 
     def edit
       @order = Spree::Order.friendly.find(params[:id])
-
+      render :edit
     end
 
     def update
@@ -41,6 +44,11 @@ module Spree
 
     def order_params
       params.require(:order).permit()
+    end
+
+    def ensure_customer
+      @order = Spree::Order.friendly.find(params[:id])
+      redirect_to login_url unless current_spree_user.customer_id == @order.customer_id
     end
   end
 end
