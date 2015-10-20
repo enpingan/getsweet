cad78f050b39c078e7a3844ff854925bb13d92b6
Ed Chang <t.edward.chang@gmail.com>
2015-10-19 23:13:20 -0400

    Making vendor id and customer id editable in admin user screens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  Spree::PermittedAttributes.user_attributes << :vendor_id
+  Spree::PermittedAttributes.user_attributes << :customer_id
+
 	include Spree::BaseHelper
 	include Spree::Core::ControllerHelpers
   include Spree::Core::ControllerHelpers::Store
