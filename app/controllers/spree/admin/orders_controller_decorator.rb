db1e2bc29d8a8c2cf8c009c4b9b8897831dd2f60
Greg Kane <gbkane@gmail.com>
2015-10-23 10:55:52 -0400

    Fix bug for going to new order in admin portal

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -5,10 +5,11 @@ Spree::Admin::OrdersController.class_eval do
     @order.update_attribute(:delivery_date, params[:order][:delivery_date])
     redirect_to :back
   end
-
+  #
   private
   def order_params
-    params.require(:order).permit(:customer_id, :delivery_date,
+    params[:created_by_id] = try_spree_current_user.try(:id)
+    params.permit(:customer_id, :delivery_date,
       :user_id, :created_by_id)
   end
 
