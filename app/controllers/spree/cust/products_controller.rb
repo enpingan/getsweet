b0a057bee7b89da7ff2faec2a75d14a31c3ffafa
Ed Chang <t.edward.chang@gmail.com>
2015-11-02 21:49:29 -0500

    Enabling images

--- app/controllers/spree/cust/products_controller.rb
+++ app/controllers/spree/cust/products_controller.rb
@@ -7,15 +7,14 @@ module Spree
       #@vendor = Spree::Vendor.friendly.find(params[:vendor_id])
       #@products = @vendor.products
       @products = Spree::Product.all
-      @vendor = Spree::Vendor.first
+      @vendor = @products.first.vendor
       render :index
     end
 
     def show
       @product = Spree::Product.friendly.find(params[:id])
 
-      @vendor = Vendor.friendly.find(params[:vendor_id])
-      # @current_order = current_customer.orders.where('vendor_id = ?', @vendor.id).last
+      @vendor = @product.vendor
       session[:vendor_id] = @vendor.id
       @current_order = current_order
 
