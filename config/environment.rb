7735d31d3918c306423fe5e449a85ef0bf9087f3
Ed Chang <t.edward.chang@gmail.com>
2015-11-05 10:50:59 -0500

    Enabling emails (devs will need to set up ENV variables allowing sendgrid access)

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base.smtp_settings = { 
+  :user_name => ENV["SENDGRID_USERNAME"],
+  :password => ENV["SENDGRID_PW"],
+  :domain => ENV["SMTP_DOMAIN"],
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # Initialize the Rails application.
-Rails.application.initialize!
+Sweet::Application.initialize!
