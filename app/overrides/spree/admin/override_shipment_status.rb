b6ffad900970b583d743ae00d28a9e01ec523e6c
Greg Kane <gbkane@gmail.com>
2015-12-04 17:47:57 -0500

    Add 'None' if no shipment state in admin view

--- app/overrides/spree/admin/override_shipment_status.rb
+++ app/overrides/spree/admin/override_shipment_status.rb
@@ -4,7 +4,7 @@ Deface::Override.new(:virtual_path => 'spree/admin/shared/_order_summary',
   :text => "<td id='shipment_state'>
               <% shipment_label = @order.shipment_state == 'received' ? 'shipped' : @order.shipment_state %>
               <span class='state label label-<%= shipment_label %>'>
-                <%= @order.shipment_state %>
+               <%= @order.shipment_state ? Spree.t(@order.shipment_state) : 'None'%>
               </span>
             </td>"
   )
